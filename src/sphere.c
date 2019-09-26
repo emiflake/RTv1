c18bfd13c6586363ac226debeff172c7c3391f19
Emily Martins <emily.flakeheart@gmail.com>
2019-09-26 20:51:37 +0200

    feat: slightly improve sphere intersection method

--- src/sphere.c
+++ src/sphere.c
@@ -6,7 +6,7 @@
 /*   By: nmartins <nmartins@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/23 16:44:57 by nmartins       #+#    #+#                */
-/*   Updated: 2019/09/23 18:25:06 by nmartins      ########   odam.nl         */
+/*   Updated: 2019/09/26 20:51:00 by nmartins      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,33 +33,30 @@ bool			sphere_intersect(
 	const t_shape *shape, const t_ray *ray, t_intersection *isect)
 {
 	const t_sphere *sphere = &shape->value.sphere;
-	t_vec3 local_ray;
-	double tca;
-	double d2;
-	double thc;
+	const t_vec3 local_sphere = vec3_sub(&ray->origin, &sphere->origin);
+	t_vec3 mul_loc;
+	double b;
+	double c;
+	double disc;
 	double t0;
 	double t1;
 	double t;
 
-	local_ray = vec3_sub(&sphere->origin, &ray->origin);
-	tca = vec3_dot(&local_ray, &ray->direction);
-	if (tca < 0.0)
+	mul_loc = vec3_multi(&local_sphere, 2.0);
+	b = vec3_dot(&mul_loc, &ray->direction);
+	c = vec3_dot(&local_sphere, &local_sphere) - (sphere->radius * sphere->radius);
+	disc = b * b - 4 * c;
+	if (disc < 0.0)
 		return (false);
-	d2 = vec3_length2(&local_ray) - tca * tca;
-	if (d2 > sphere->radius * sphere->radius)
-		return (false);
-	thc = sqrt(sphere->radius * sphere->radius - d2);
-	t0 = tca - thc;
-	t1 = tca + thc;
-	if (t0 < 0.0 && t1 < 0.0)
-		return (false);
-	if (t0 < 0.0)
-		t = t1;
-	else if (t1 < 0.0)
-		t = t0;
 	else
-		t = t0 < t1 ? t0 : t1;
-	if (t <= 0.0)
+		disc = sqrt(disc);
+	t0 = -b + disc;
+	t1 = -b - disc;
+	if (t1 > 0.0)
+		t = t1 / 2.0;
+	else if (t0 > 0.0)
+		t = t0 / 2.0;
+	else
 		return (false);
 	if (t <= isect->t)
 	{
